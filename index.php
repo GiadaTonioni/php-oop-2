572f0226680e9ec71b81e255f235b991b9519b7b
GiadaTonioni <giada.tonioni@gmail.com>
2023-07-11 16:23:41 +0200

    importati in index

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
+require_once __DIR__.'/Models/Prodotti.php';
+require_once __DIR__.'/Models/Cibo.php';
+require_once __DIR__.'/Models/Accessori.php';
+require_once __DIR__.'/Models/Giochi.php';
+
+$prodotti = new Prodotti('Test prodotto', 'httpsimmagine.jpg', 34.10, 'Cane');
+ var_dump($prodotti)
+
 
 ?>
 
